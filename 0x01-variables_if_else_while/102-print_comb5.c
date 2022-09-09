ab26ae968c50fcf85546750752c94e8427c7e0c5
Mohammed Abdella <moh.abd94@gmail.com>
2022-09-09 05:36:37 -0700

    Last advanced task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,19 +9,28 @@ int main(void)
 {
 	int a;
 	int b;
+	int c;
+	int d;
 
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			if (a < b && a != b)
+			for (c = a; c <= 9; c++)
 			{
-				putchar(a + '0');
-				putchar(b + '0');
-				if (a == 8 && b == 9)
-					continue;
-				putchar(',');
-				putchar(' ');
+				for (d = b; d <= 9; d++)
+				{
+
+					if (a == c && b == d)
+						continue;
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(' ');
+					putchar(c + '0');
+					putchar(d + '0');
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
