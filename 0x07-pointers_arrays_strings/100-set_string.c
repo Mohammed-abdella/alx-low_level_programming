f460d0b71dd03152e0641fa723419f1e6600213c
Mohammed Abdella <moh.abd94@gmail.com>
2022-09-26 10:29:08 -0700

    Advanced tasks of 0x07

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - to set the value of a pointer to char
+ * @a: the input pointer
+ * @b: the input character
+ */
+void set_string(char **a, char *b)
+{
+	*a = b;
+}
